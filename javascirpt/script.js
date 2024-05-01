7e42bee6905c9fb5ad995c224f3664721b305a7f
Hadiya204 <168410223+Hadiya204@users.noreply.github.com>
2024-05-01 16:06:48 +0500

    uploaded

---
+++ javascirpt/script.js
@@ -0,0 +1,26 @@
+alert("welcome to our website");
+var first_name = "Hadiya khan";
+console.log(first_name);
+var first_name = 'khan';
+console.log(first_name);
+let last_name = "hellow";
+console.log(last_name);
+var first_name;
+first_name = 'hadiya';
+var number = '2021';
+console.log(number);
+var text =  "it is invited in 1845"
+console.log(text)
+var name = "hadiya khan";
+ var email = "HADIYAKHAN@GMAIL.COM";
+var dateofbirth= "2003";
+console.log(name + email + dateofbirth);
+console.log(email);
+console.log(dateofbirth);
+alert(name + "" + email+ "" +dateofbirth);
+console.log(email);
+console.log(dateofbirth);
+let a = 12;
+let b = 16;
+let result = a + b;
+console.log("the sum of two number is:"+ (a + b));
